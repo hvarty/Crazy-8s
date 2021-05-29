68c42e21d5c0497998d1028f5fb4807b0f7c6127
hvarty <hvarty@tcs.on.ca>
2021-05-28 21:44:57 -0400

    save

--- src/play.js
+++ src/play.js
@@ -1,24 +1,64 @@
+import './play.css'
+import { shuffle } from "./shuffle"
+
+
 import { useState, useEffect } from 'react'
-const play = () => {
-    const [mainDeck, setmainDeck] = useState([]);
+const Play = () => {
+    const [mainDeck, drawPile] = useState([]);
     const [hand, sethand] = useState([]);
     const [computerHand, setcomputerHand] = useState([]);
-    const [playedDeck, setcomputerHand] = useState([]);
-    useEffect(()=>{
+    const [playedDeck, setplayedDeck] = useState([]);
+
+    useEffect(() => {
         //runs on startup only
         //initialize main deck
-        const tempDeck=[]
-        for (let i=0;i<52; i++){
-            tempDeck.push(i);
-        }
-        setmainDeck(tempDeck);
+        let tempDeck = []
+
+        const suits = ["clubs", "spades", "hearts", "diamonds"]
+        suits.forEach((suit) => {
+            //init the deck
+            //Ace = 1
+            // 2->10
+            //11 = Jack, 12 = Queen, 13 = King 
+            for (let i = 1; i < 14; i++) {
+                tempDeck.push({
+                    suit: suit,
+                    number: i
+                });
+            }
+        })
+
+
+        //shuffle the deck first...
+        tempDeck = shuffle(tempDeck)
+        console.log(tempDeck)
+
+
+
+        //deal hands
+        const receiveCards = tempDeck.splice(0, 8)
+        const giveCards = tempDeck.splice(0, 8)
+
+        //set hands and drawpile
+        sethand(receiveCards)
+
+        setcomputerHand(giveCards)
+
+        drawPile(tempDeck)
 
         //initialize hand
         //take the cards in the value of 0-7 from the card array and set those as the value for hand 
         //take the cards in the value of 8-15 from the card array and set those as the value for Computerhand
+    }, [])
 
-        
-    },[])
+    const playCard=(card)=>{
+        console.log('play card', card)
+    }
+
+    
+
+
+
     //turn method that checks if the card played is allowed and if there is any special thing that needs to happen
     //such as 2 is pick up 2 and 8 is change the suit
 
@@ -32,7 +72,7 @@ const play = () => {
 
     //computer turn selects a random card from the computer hand array and loops the turn method to check 
     //if the card can be played until it reaches the last card and picks up if needed
-    
+
     //in turn method if an 8 is played, a pop-up input appears asking for a choice between 4 suits
     //if the computer plays an 8, it selects a suit randomly
 
@@ -42,8 +82,71 @@ const play = () => {
 
     const dealACard = () => {
     }
-    return(
-        <div>play</div>
+    return (
+        <div>
+            <div className='hand'> 
+                <div>hand</div>
+
+                <div className='cards'>
+                    {hand.map((card,index) => {
+                        return (
+                            <div className='card' onClick={()=>playCard(card)} key={index}>
+                            
+                                <img src={'/cards/' + card.number + card.suit.substring(0,1).toUpperCase() + '.svg'}/>
+                                
+                             
+                            </div>
+
+                        )
+                    })
+
+                    }
+                </div>
+
+            </div>
+
+
+        </div>
     )
 }
-export default play
+
+export default Play
+
+//GAME LOOP- everything is inside this and it is activated from the play button on the intro screen,
+        //at the end if this loop before it repeats, it checks if the active player has 0 cards left, 
+        //if they do, they win
+
+//TURN- when the user clicks a card, turn checks if it can be played by comparing it to the top card's value
+        //if the card cannot be played, text pop-up saying, you cannot play that card, appears, this
+        //will disappear if another card is played that works, or stay if another wrong card is played
+        //each turn ends when either correctcard played is true or the draw pile is clicked
+        //if the draw pile is clicked, the pop method is used from the cards in the draw pile array and it is
+        //added to the hand
+
+        //turn only ends when play cards button is clicked, allowing for mutliple cards to be checked
+        //in one turn
+
+        //if computer's turn is true, the program selects a random card from the computer hand array 
+        //and loops the check method to check if the card can be played until it reaches the last card 
+        //and picks up if needed
+
+        //need a check function that compares card to the topcard
+        //need an if statement that checks a turn boolean the determines which player is currently playing
+        //need play card button
+
+        //if Check is true after the play card button is clicked, these selected card(s) are moved to 
+        //played pile's array
+
+//SPECIAL (within TURN right after CHECK is true)- if a 2 with the matching suit of the top card is played
+        //the oposing player gains the top 2 cards, if multiple 2's are played and check is true, multiply
+        //2 by the number of 2's played and add that amount of cards from the top of the draw pile to the
+        //oposition's hand
+
+        //if an 8 is played (or multiple) the pop-up occurs and gives a choice of suit, this changes the
+        //'top card' variable which is normally taking the value from the card, and making the suit take
+        //the value from the suit input
+
+
+//SHUFFLE - if the draw pile runs out, the played card pile leaves the top card and shuffles the rest
+        //then it puts this array of cards into the draw pile, a 'shuffling cards' pop up occurs
+
---
+++ src/shuffle.js
@@ -0,0 +1,25 @@
+/**
+ * Shuffle an array
+ * @author coolaj86 https://stackoverflow.com/users/151312
+ * FROM: https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+ * @param {*} array 
+ * @returns array
+ */
+export function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+  
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+  
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+  
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+  
+    return array;
+  }
